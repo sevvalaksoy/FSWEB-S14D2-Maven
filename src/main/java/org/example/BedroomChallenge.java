e72a132620ba5accd167a068a1acff2516282540
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-07-30 13:11:42 +0300

    Challenge

--- src/main/java/org/example/BedroomChallenge.java
+++ src/main/java/org/example/BedroomChallenge.java
@@ -1,21 +1,25 @@
 package org.example;
 
 import org.example.model.*;
+import org.example.model.enums.RoomType;
 
 public class BedroomChallenge extends Room {
+    private RoomType name = RoomType.BEDROOM;
     private Bed bed;
     private Lamp lamp;
     private Wardrobe wardrobe;
     private Carpet carpet;
 
-    public BedroomChallenge(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet){
-        super(name, wall1, wall2, wall3, wall4, ceiling);
+    public BedroomChallenge(Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet){
+        super(wall1, wall2, wall3, wall4, ceiling);
         this.bed = bed;
         this.lamp = lamp;
         this.wardrobe = wardrobe;
         this.carpet = carpet;
     }
-
+    public RoomType getName(){
+        return name;
+    }
     public Bed getBed(){
         return bed;
     }
@@ -31,8 +35,9 @@ public class BedroomChallenge extends Room {
 
     @Override
     public String toString() {
-        return  super.toString() + "BedroomChallenge{" +
-                "bed=" + bed +
+        return  super.toString() + " BedroomChallenge{" +
+                "name=" + name +
+                ", bed=" + bed +
                 ", lamp=" + lamp +
                 ", wardrobe=" + wardrobe +
                 ", carpet=" + carpet +
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,16 @@ public class Main {
                 new Ceiling(300, PaintColor.WHITE), new Bed("Soft",2,200,1,1), new Lamp(LampType.LAVA, false, 10),
                 new Wardrobe(200,200,300), new Carpet(150,150,PaintColor.RED));
         System.out.println(bedroom1);
+
+        BedroomChallenge bedroom2 = new BedroomChallenge(new Wall("South"), new Wall("North"), new Wall("East"), new Wall("West"),
+                new Ceiling(300, PaintColor.RED), new Bed("Soft",2,200,1,1), new Lamp(LampType.LAVA, false, 10),
+                new Wardrobe(200,200,300), new Carpet(150,150,PaintColor.WHITE));
+
+        System.out.println(bedroom2);
+
+        Room room = new Room(new Wall("South"), new Wall("North"), new Wall("East"), new Wall("West"),
+                new Ceiling(300, PaintColor.GREEN));
+
+        System.out.println(room);
     }
 }
